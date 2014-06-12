c135af083eea54c3615693d02c6c3fe2f689d729
Stuart Lee <stuart.lee@drpgroup.com>
2014-06-12 09:49:59 +0100

    working on ai

--- src/AI.js
+++ src/AI.js
@@ -1,146 +1,66 @@
 
+/**
+ * NEW AI TESTS
+ */
 
-var AiManager = function() {
 
-    this.counter = 0;
-    this.interval = 1000;
+function aaaiii(entity) {
+
+    // choose highest priority target
+    // for now will just get the player every time
+    // if i add multiple factions it may get expanded
+    // @todo expand
+    var range = 150;
+    var possibleTargets = getTargetsInRange(entity, range);
+
+    if(possibleTargets.length > 0) {
+    
+        var target = possibleTargets[0];
+    
+        for( var i = 1; i < possibleTargets.length; i++) {
+            // compare to current target and decide if more important
+        }
+    
+        // run away from target
+        // var direction = entity.position._subtract(target.position).normalise();
+        var direction = target.position._subtract(entity.position).normalise();
+    
+        entity.velocity.set( direction.x, direction.y );
+    }
     
 };
 
+function getTargetsInRange(entity, range) {
 
-AiManager.prototype.update = function(delta) {
+    // @todo get all possible targets properly
+    var possible = [player];
     
-    this.counter += delta;
+    var targets = [];
     
-    while(this.counter > this.interval) {
-     
-        this.counter -= this.interval;
-        
-        think();
-        
+    for( var i = 0; i < possible.length; i++)
+    {    
+        if( entity.position.distanceTo( possible[i].position ) < range ) {
+            targets.push( possible[i] );
+        }
     }
     
+    return targets;
 };
 
 
 
-function think() {
-    
-    var threat = getThreat();
-    
-    if(threat) {
- 
-        if( shouldAttack(threat) ) {
-         
-            attack(threat);
-            
-        } else {
-         
-            // will keep going until the threat is no longer present
-            flee(threat);
-            
-        }
-        
-    } else {
-        
-        wander();
-        
-    }
-
-}
-
-
-// look for local threats, if more than one, get most pressing
-function getThreat() {
- 
-    var threats = getThreats();
-    
-    if(threats.length == 1) {
-    
-        return threats[0];
-    
-    } else if(threats.length > 1) {
-        
-        var highest = threats[0];
-        
-        // find the highest
-        for(var i = 1; i < threats.length; i++) {
-         
-            if( threatLevel(threats[i]) > threatLevel(highest) ) {
-                
-                highest = threats[i];
-            
-            }
-        
-        }
-        
-        return highest;
-        
-    }
-    
-    return false;
-    
-}
-
-// just ranks player top for now
-function threatLevel(target) {
- 
-    if(target == player) {
-     
-        return 2;
-        
-    }
-    
-    return 1;
-    
-}
 
 
 
-function wander() {
- 
-    var target = somewhereRandom();
-    
-    this.setTarget(target);
-    
-}
 
-function flee(target) {
- 
-    var direction = opposite(target);
-    
-    this.velocity = direction.normalise();
-    
-}
 
-function shouldAttack(target) {
- 
-    // dont bother if I have been too badly hurt
-    // warning is set to 0 for stupid enemies
-    if(this.health < this.healthWarningLevel) {
-     
-        return false;
-        
-    }
-    
-    return true;
-    
-}
 
-function attack(target) {
-    
-    var range = rangeTo(target);
-    
-    if(range > this.weapon.range) {
-    
-        moveTowards(target);
-        
-    } else {
-     
-        // do whatever the weapon does (bullet etc)
-        weapon.attack(target);
-        
-    }
-    
-}
+
+
+
+
+
+
+
+
 
