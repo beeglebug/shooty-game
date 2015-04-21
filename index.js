ab5a3013ee6eae16bcb421e2dc1f57715404b494
Stuart Lee <stuart.lee@drpgroup.com>
2015-04-21 22:19:27 +0100

    moved stuff around

---
+++ index.js
@@ -0,0 +1,37 @@
+var PIXI = require('pixi.js');
+var Input = require('js-game-lib').Input;
+
+var Stage = PIXI.Stage;
+var Renderer = PIXI.WebGLRenderer;
+var DisplayObjectContainer = PIXI.DisplayObjectContainer;
+
+// pixel goodness
+PIXI.scaleModes.DEFAULT = PIXI.scaleModes.NEAREST;
+
+var width = 800;
+var height = 450;
+var stage = new Stage(0x0000000);
+var renderer = new Renderer( width, height );
+//var world = new DisplayObjectContainer();
+
+
+// dom binding
+var mount = document.getElementById('app-mount');
+mount.appendChild(renderer.view);
+
+Input.bindKeyboard(document.body);
+Input.bindMouse(document.body);
+
+
+//// build map
+//var importer = new TiledImporter();
+//
+//importer.addEventListener('complete', function(event) {
+//
+//    map = event.content;
+//
+//    loadMap(map);
+//
+//});
+//
+//importer.load('assets/map.json');
