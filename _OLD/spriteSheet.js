ac4d103079c44d46ca6ccae19611a842778c0755
Stuart Lee <stuart.lee@drpgroup.com>
2015-04-21 22:38:23 +0100

    moving old files

---
+++ _OLD/spriteSheet.js
@@ -0,0 +1,30 @@
+// @todo tileset object again
+// needs an offset
+
+
+
+
+function makeSpriteSheet( baseTexture, width, height ) {
+
+	var textures = [],
+		texture, x, y;
+
+    for ( y = 0; y < baseTexture.height; y += height ) {
+		
+		for ( x = 0; x < baseTexture.width; x += width ) {
+        
+			texture = new PIXI.Texture( baseTexture );
+
+			texture.frame.width = width;
+			texture.frame.height = height;
+			texture.frame.x = x;
+			texture.frame.y = y;
+
+			textures.push(texture);
+		}
+	
+	}
+	
+	return textures;
+	
+}
